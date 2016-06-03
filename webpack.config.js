9eed30dfadf14543c29deb3ef28ecd3fb353902f
MartinMuzatko <martin@happy-css.com>
2016-06-03 17:01:37 +0200

    Added webpack dev server settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 var webpack = require('webpack')
+var path = require('path')
 
 module.exports = {
     entry: './index.js',
     output: {
         path: './',
         filename: 'bundle.js',
-        publicPath: './'
+        publicPath: ''
     },
     plugins: [
         new webpack.ProvidePlugin({
@@ -22,5 +23,11 @@ module.exports = {
             { test: /\.less$/, loader: 'style!css!less' },
             { test: /\.js$/, loader: 'babel', query: { presets: 'es2015-riot' } }
         ]
-    }
+    },
+    devServer: {
+        port: 7070,
+        outputPath: __dirname,
+        inline: false,
+        progress: true,
+    },
 }
