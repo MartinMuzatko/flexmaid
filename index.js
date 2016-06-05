3dae47ec0e3494ec4eb9ee65ae863bb0d7a3cc8e
Martin Muzatko <martin@happy-css.com>
2016-06-05 21:35:53 +0200

    Added nouislider dependency

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import 'riot'
+var nouislider = require('nouislider')
 
 import './tags/app.html'
 import './tags/breakpoint.html'
 
 import './less/main.less'
 
+riot.mixin('nouislider', {nouislider:nouislider}, true)
 riot.mount('*')
